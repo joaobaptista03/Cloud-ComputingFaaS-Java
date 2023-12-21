8c88bfc26602946b9ebac56e8c1af1a56d26e4d3
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2023-12-21 18:54:12 +0000

    Add equals() and hashCode() methods to User class

--- Código/User.java
+++ Código/User.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class User {
     private String username;
     private String password;
@@ -13,5 +15,16 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User otherUser = (User) obj;
+        return Objects.equals(username, otherUser.username);
+    }
+
+    public int hashCode() {
+        return Objects.hash(username);
     }
 }
