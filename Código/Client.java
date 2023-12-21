c45b02954bf4642a4d0d2ea94bec2c2dd7f4eec2
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2023-12-21 13:21:29 +0000

    imports

--- Código/Client.java
+++ Código/Client.java
@@ -1,4 +1,9 @@
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.util.Scanner;
